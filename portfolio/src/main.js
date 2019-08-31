dc960bfd371ab6e65eaed020576bc2fc308272d4
adrian <cr7adrian@hotmail.com>
2019-08-31 14:21:02 -0400

    Vue routing package added

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,13 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VueRouter from 'vue-router';
 
+import ProjectBox from "./components/ProjectBox";
+import BaseComponent from "./components/BaseComponent";
 
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  base: '/',
+  mode: "history",
+  routes: [
+    {path:'/', component: BaseComponent},
+    {path: '/projects', component: ProjectBox}
+  ] // short for `routes: routes`
+});
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+   render: h => h(App),
+   router,
+}).$mount('#app');
