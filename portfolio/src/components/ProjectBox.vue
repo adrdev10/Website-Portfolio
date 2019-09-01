40893ba172a63a4856e565b03fdf8ab802c75ac6
adrian <cr7adrian@hotmail.com>
2019-09-01 00:17:51 -0400

    using props from the BaseComponent to this one

--- portfolio/src/components/ProjectBox.vue
+++ portfolio/src/components/ProjectBox.vue
@@ -1,19 +1,8 @@
 <template>
   <div>
-    <b-card-group deck>
-      <b-card
-        header="featured"
-        header-tag="header"
-        footer="Card Footer"
-        footer-tag="footer"
-        title="Title"
-      >
-        <b-card-text>Header and footers using props.</b-card-text>
-        <b-button href="#" variant="primary">Go somewhere</b-button>
-      </b-card>
-
-      <b-card title="Title" header-tag="header" footer-tag="footer">
-        <h6 slot="header" class="mb-0">Header Slot</h6>
+    <b-card-group deck >
+      <b-card title="Title" header-tag="header" footer-tag="footer" v-for="(item, index) in data" v-bind:key="index">
+        <h6 slot="header" class="mb-0">{{item.name}}</h6>
         <b-card-text>Header and footers using slots.</b-card-text>
         <b-button href="#" variant="primary">Go somewhere</b-button>
         <em slot="footer">Footer Slot</em>
@@ -27,11 +16,18 @@ export default {
     data() {
         return {}
     },
-
-    // props:[dataArray],
+    props: {
+      data: Array,
+    }
 };
 </script>
 
 
 <style scoped>
+h6{
+  color: black;
+}
+b-card-group b-card {
+    max-width: 25%;
+}
 </style>
