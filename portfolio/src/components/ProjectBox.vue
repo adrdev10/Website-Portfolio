76ca6f9d2b97ce129a1acbf259b2f04fc479bf69
adrian <cr7adrian@hotmail.com>
2019-09-03 12:59:32 -0400

    H2 header tag now uses link a tag

--- portfolio/src/components/ProjectBox.vue
+++ portfolio/src/components/ProjectBox.vue
@@ -1,33 +1,76 @@
 <template>
-  <div>
-    <b-card-group deck >
-      <b-card title="Title" header-tag="header" footer-tag="footer" v-for="(item, index) in data" v-bind:key="index">
-        <h6 slot="header" class="mb-0">{{item.name}}</h6>
-        <b-card-text>Header and footers using slots.</b-card-text>
-        <b-button href="#" variant="primary">Go somewhere</b-button>
-        <em slot="footer">Footer Slot</em>
-      </b-card>
-    </b-card-group>
+  <div id="app-project">
+    <div id="app-children" v-for="(item, index) in data" v-bind:key="index">
+      <ul>
+        <li>
+          <a v-bind:href="item.html_url"><h2>{{item.name}}</h2></a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {}
-    },
-    props: {
-      data: Array,
-    }
+  data() {
+    return {};
+  },
+  props: {
+    data: Array
+  }
 };
 </script>
 
 
-<style scoped>
-h6{
-  color: black;
+<style>
+@import url("https://fonts.googleapis.com/css?family=Anton&display=swap");
+
+html {
+  height: 100%;
 }
-b-card-group b-card {
-    max-width: 25%;
+
+#app-project {
+  font-family: "Helvetica";
+  text-transform: uppercase;
+  color: #333333;
+  font-weight: 500;
+  line-height: 2.15;
+  position: absolute;
+  margin: 0 auto;
+  top: 20%;
+  bottom: 30%;
+  background-color: black;
+  letter-spacing: 2.10px;
+  width: 100%;
+  height: 100%;
+}
+
+a {
+  color: #333333 !important;
+  text-decoration: none;
+  cursor: default !important;
+}
+
+#app-children {
+  position: relative;
+}
+
+a:hover {
+  cursor: default !important;
+  color: white !important;
+}
+
+li:hover {
+    cursor: default !important;
+  color: white !important;
+}
+ 
+ul {
+  list-style: none
+}
+
+h2,
+.h2 {
+  font-size: 3.5rem !important;
 }
 </style>
